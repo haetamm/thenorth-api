36e4749fbcbec6449ce4ccea87d281f9b5ecfd44
Thaetami <tatanghaetami.97@gmail.com>
2024-05-03 13:52:57 +0700

    update database config

--- src/config/database.js
+++ src/config/database.js
@@ -3,31 +3,13 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    // "username": process.env.DB_USER,
-    // "password": process.env.DB_PASS,
-    // "database": process.env.DB_NAME,
-    // "host": process.env.DB_HOST,
-    // "dialect": "mysql",
-    // "dialect": "sqlite",
-    // "storage": "../db/database.sqlite"
-    "username": process.env.DB_USER_DEPLOYMENT,
-    "password": process.env.DB_PASS_DEPLOYMENT,
-    "database": process.env.DB_NAME_DEPLOYMENT,
-    "host": process.env.DB_HOST_DEPLOYMENT,
-    "dialect": "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true, // Ini mungkin diperlukan tergantung setup Supabase
-        rejectUnauthorized: false // Diperlukan jika menggunakan self-signed certificates
-      }
-    }
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
   },
   "test": {
-    // "username": process.env.DB_USER,
-    // "password": process.env.DB_PASS,
-    // "database": process.env.DB_NAME,
-    // "host": process.env.DB_HOST,
-    // "dialect": "mysql",
     "dialect": "sqlite",
     "storage": "../db/database.sqlite"
   },
